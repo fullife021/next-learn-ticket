fec1b383754c1532f2b06c0979a5313c5c3a7610
user <user@userdeMacBook-Pro.local>
2024-03-27 04:50:04 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        API_ADDRESS:"https://next-learn-ticket-cpnp8cld6-77266089-qqcom.vercel.app/api"
+        API_ADDRESS:"https://next-learn-ticket-gamma.vercel.app/api"
     }
 };
 
